d9abac878f6a11a7b56d0d8129ca02f2014508bd
Savil Kumar <savil.kumar@borngroup.com>
2024-09-19 09:15:47 +0530

    first

---
+++ rewards_app/lib/rewards_app.dart
@@ -0,0 +1,13 @@
+library rewards_app;
+
+import 'package:flutter/cupertino.dart';
+import 'package:rewards_app/pages/home.dart';
+
+class RewardsApp extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context) {
+    return RewardsPage();
+  }
+
+}
---
+++ rewards_app/test/rewards_app_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:rewards_app/rewards_app.dart';
+
+void main() {
+
+}
